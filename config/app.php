9e25eed377e6188db9aac8d015e447ad6f499b40
satishkumarpawar <satishkumarpawar@gmail.com>
2022-07-04 12:30:20 +0530

    app url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://api.starsgyan.com/ecommerce_repo'),
 
     /*
     |--------------------------------------------------------------------------
