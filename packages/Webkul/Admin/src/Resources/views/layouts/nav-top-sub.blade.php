2f20e5822b63e2f2a4d012cd7d2ce4fd2b1c66dd
satishkumarpawar <satishkumarpawar@gmail.com>
2022-07-06 16:00:46 +0530

    admin menus modified to show additinal menus

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-top-sub.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-top-sub.blade.php
@@ -28,11 +28,34 @@
                             <div class="dropdown-container">
                                 <ul>
                                     @foreach ($menuItem['children'] as $key => $item)
+                                        @if(count($item['children']))
+                                            <li style="border-bottom:1px solid #e8e8e8; " class="topmenu {{$menu->getActive($item)}}">
+                                                <a class="{{$menu->getActive($item)}}" href="{{ $item['url'] }}">
+                                                {{ isset($item['name']) ? trans($item['name']) : '' }}
+                                                </a>
+                                            </li> 
+                                            <li >
+                                           
+                                                <ul>
+                                                    @foreach ($item['children'] as $key => $item2)
+                                                        <li style="background-color:#fbfbfb; margin-top:2px; border-radius:2px; padding-left:5px;" class="submenu {{$menu->getActive($item2)}}">
+                                                            <a class="{{$menu->getActive($item2)}}" href="{{ $item2['url'] }}">
+                                                            {{ isset($item2['name']) ? trans($item2['name']) : '' }}
+                                                            </a>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                               
+                                            </li>
+                                        @else
                                         <li class="topmenu {{$menu->getActive($item)}}">
                                             <a class="{{$menu->getActive($item)}}" href="{{ $item['url'] }}">
                                              {{ isset($item['name']) ? trans($item['name']) : '' }}
                                             </a>
                                         </li>
+                                            
+                                        @endif
+                                       
                                     @endforeach
                                 </ul>
                             </div>
