a3b8d32b0aebb7b16f2e42d0022a2933411d8ade
satishkumarpawar <satishkumarpawar@gmail.com>
2022-07-15 18:15:03 +0530

    modify logo size

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-top.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-top.blade.php
@@ -5,7 +5,7 @@
                 @if (core()->getConfigData('general.design.admin_logo.logo_image', core()->getCurrentChannelCode()))
                     <img src="{{ \Illuminate\Support\Facades\Storage::url(core()->getConfigData('general.design.admin_logo.logo_image', core()->getCurrentChannelCode())) }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
                 @else
-                    <img src="{{ asset('vendor/webkul/ui/assets/images/admin-logo.png') }}" alt="{{ config('app.name') }}"/>
+                    <img src="{{ asset('vendor/webkul/ui/assets/images/admin-logo.png') }}" style="max-height:50px;" alt="{{ config('app.name') }}"/>
                 @endif
             </a>
         </div>
