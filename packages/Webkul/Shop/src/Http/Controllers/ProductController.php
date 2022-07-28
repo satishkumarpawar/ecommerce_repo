8efc498a4c5605ca5fed2c2737cd536ed66a337d
satishkumarpawar <satishkumarpawar@gmail.com>
2022-07-28 16:47:40 +0530

    pdf view url validation

--- packages/Webkul/Shop/src/Http/Controllers/ProductController.php
+++ packages/Webkul/Shop/src/Http/Controllers/ProductController.php
@@ -135,7 +135,7 @@ class ProductController extends Controller
         $this->validate(request(), [
             'url'            => 'required|url',
         ]);
-        if(strpos(request()->url,App::make('url')->to('/'))!==true){
+        if(strpos(request()->url,$_SERVER['HOST_NAME'])!==true){
             abort(404);
         }
         return view($this->_config['view'])->with('file_url',request()->url );
